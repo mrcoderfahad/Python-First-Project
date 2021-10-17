f9632308646a3f5cfd0646dd804dd889d15f07ec
fahad rifat <fahadrifat1@gmail.com>
2021-10-17 20:39:04 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+# students_count = 1000
+# print(students_count)
+
+# rating = 5
+# print(rating)
+
+# my_name = True
+# print(my_name)
+
+# full_name = "Abdullah Fahad Rifat"
+# print(len(full_name))
+
+mylist = "Abdullah Fahad Rifat"
+print(len(mylist))
+print(mylist[0])
+print(mylist[-1])
+print(mylist[0:6])
+print(mylist[0:])
